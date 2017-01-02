b4c036a15afa5dfdd8e043e61b1cbd450cb019b0
bumsoo <meliketoy@gmail.com>
2017-01-02 18:43:40 +0900

    cropping

--- datasets/cifar10.lua
+++ datasets/cifar10.lua
@@ -43,10 +43,9 @@ local meanstd = {
 function CifarDataset:preprocess()
     if self.split == 'train' then
         return t.Compose{
-            t.Scale(36),
             t.ColorNormalize(meanstd),
             t.HorizontalFlip(0.5),
-            t.RandomCrop(32, 0),
+            t.RandomCrop(32, 4),
         }
     elseif self.split == 'val' then
         return t.ColorNormalize(meanstd)
