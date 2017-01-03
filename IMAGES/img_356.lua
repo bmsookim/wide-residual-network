75f8ff5a39ff4d06fbfea653e543022992f5e6a8
bumsoo <meliketoy@gmail.com>
2017-01-03 13:02:58 +0900

    image converting

--- IMAGES/img_356.lua
+++ IMAGES/img_356.lua
@@ -8,7 +8,9 @@ new_img = image.load(new_path)
 print(img:size())
 print(new_img:size())
 
--- new_img:resizeAs(img)
--- image.save(new_path, new_img)
+trans_img = image.scale(new_img, img:size(2), img:size(3), 'bicubic')
+image.save(new_path, trans_img)
 
-
+print("Image Convert Completed!")
+print(img:size())
+print(trans_img:size())
